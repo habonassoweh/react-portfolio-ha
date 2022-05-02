76841f5b5a1082700aac30298572b978ab95d6ad
Habon Assoweh <habonassoweh@Habons-Air>
2022-05-02 01:56:23 -0400

    Contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,40 +2,64 @@ import React from 'react'
 
 import "./contact.css"
 
+function Contact() {
 
-const Contact = () => {
- 
+  // JSX
   return (
-    <div className="contact">
-      <div className="contact-bg"></div>
-      <div className="contact-wrapper">
-        <div className="contact-left">
-          <h1 className="contact-title">Let's Chat</h1>
-          <div className="contact-info">
-            <div className="contact-info-item">
-              {/* <img src="/img/contact-image.jpg" alt="" className='contact-img'/> */}
-            </div>
-          </div>
+    <section>
+      <h1>Contact me</h1>
+      <form id="contact-form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" />
         </div>
-        <div className="contact-right">
-          <p className='contact-des'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id pariatur 
-            laudantium corporis, provident dolorum excepturi.
-          </p>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+    );
+  }
+
+// const Contact = () => {
+ 
+//   return (
+//     <div className="contact">
+//       <div className="contact-bg"></div>
+//       <div className="contact-wrapper">
+//         <div className="contact-left">
+//           <h1 className="contact-title">Let's Chat</h1>
+//           <div className="contact-info">
+//             <div className="contact-info-item">
+//               {/* <img src="/img/contact-image.jpg" alt="" className='contact-img'/> */}
+//             </div>
+//           </div>
+//         </div>
+//         <div className="contact-right">
+//           <p className='contact-des'>
+//             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id pariatur 
+//             laudantium corporis, provident dolorum excepturi.
+//           </p>
          
-           <input type="text" placeholder='Name' name="user_name" />
-           <input type="text" placeholder='Email' name="user_email" />
-           <textarea placeholder="Message" rows="10" name="message" ></textarea>
-           <button>Submit</button>
+//            <input type="text" placeholder='Name' name="user_name" />
+//            <input type="text" placeholder='Email' name="user_email" />
+//            <textarea placeholder="Message" rows="10" name="message" ></textarea>
+//            <button>Submit</button>
 
           
-        </div>
-      </div>
+//         </div>
+//       </div>
        
 
            
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
 export default Contact
--- src/components/portfolio/PhotoList.jsx
+++ src/components/portfolio/PhotoList.jsx
@@ -1,45 +1,62 @@
 import React, {useState} from 'react'
 
-import photo from '../../images/1.png';
+import clouds from '../../images/1.png';
+
+import sparkly from '../../images/2.png';
+
+import greencloud from '../../images/3.png';
+
+import bluecloud from '../../images/4.png';
+
+import pastel from '../../images/5.png';
+
+import pinkcloud from '../../images/6.png';
 
 
 function PhotoList() {
     const [photos] = useState([{
         name:"project 1",
         category:"portfolio",
-        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam."
+        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam.",
+        variable: clouds
     },
     {
         name:"project 2",
         category:"portfolio",
-        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam."
+        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam.",
+        variable: sparkly
     },
     {
         name:"project 3",
         category:"portfolio",
-        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam."
+        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam.",
+        variable: greencloud
     },
     {
         name:"project 4",
         category:"portfolio",
-        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam."
+        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam.",
+        variable: bluecloud
     },
     {
         name:"project 5",
         category:"portfolio",
-        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam."
+        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam.",
+        variable: pastel
     },
     {
         name:"project 6",
         category:"portfolio",
-        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam."
+        describe: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ullam.",
+        variable: pinkcloud
     }])
     return (
       <div>
           <div className='flex-row'>
               {photos.map((image, i) => (
                   <img
-                  src={require(`../../images/${category}/${i}.jpg`).default}
+                  src={image.variable}
+
                   alt={image.name}
                   className="img-thumbnail mx-1"
                   key={image.name}
@@ -47,7 +64,7 @@ function PhotoList() {
               ))}
           </div>
           <img
-            src={photo}
+            src={clouds}
             alt="Portfolio Example"
           />
       </div>
